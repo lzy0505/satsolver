eedc10f5decdcf796687cef2a9710d984bfeec0d
Zongyuan Liu <liuzongyuan0505@gmail.com>
2019-12-03 13:30:07 +0100

    finish proof

--- Solver.v
+++ Solver.v
@@ -192,7 +192,45 @@ Definition solver (p : form) :Datatypes.bool:=
   end.
 
 
-Lemma and_interp: forall p1 p2 V, interp V p1=true /\ interp V p2=true <-> interp V (land p1 p2)=true.
+
+
+(*
+Lemma find_val_rec : forall p v v' l, interp v p = true \/ (interp v p = false /\ try_valuation l p= Some v' ) ->  (exists V, try_valuation (v::l) p=Some V).
+Proof.
+  intros.
+  destruct H.
+  - exists v.
+    unfold try_valuation.
+    now rewrite H.
+  - exists v'.
+    unfold try_valuation.
+    destruct H.
+    rewrite H.
+    unfold try_valuation in H0.
+    assumption.
+Qed.
+
+
+  
+
+Lemma and_solver: forall p1 p2 V, find_valuation (land p1 p2)=Some V -> satisfiable p1.
+Proof.
+  intros.
+    unfold find_valuation in H.
+    unfold try_valuation in H.
+    induction (enum_valuation (collect_var (land p1 p2))). 
+    + discriminate H.
+    +  destruct (interp a (land p1 p2)) eqn: E1.
+       * unfold satisfiable.
+         exists a.
+         apply and_interp in E1.
+         destruct E1.
+         assumption.
+       * apply IHl. (*why it works?*)
+         apply H.
+Qed.*)
+
+Lemma land_interp_interp: forall p1 p2 V, interp V p1=true /\ interp V p2=true <-> interp V (land p1 p2)=true.
 Proof.
   intros.
   split.
@@ -214,51 +252,7 @@ Proof.
       
 Qed.
 
-(*
-Lemma find_val_rec : forall p v v' l, interp v p = true \/ (interp v p = false /\ try_valuation l p= Some v' ) ->  (exists V, try_valuation (v::l) p=Some V).
-Proof.
-  intros.
-  destruct H.
-  - exists v.
-    unfold try_valuation.
-    now rewrite H.
-  - exists v'.
-    unfold try_valuation.
-    destruct H.
-    rewrite H.
-    unfold try_valuation in H0.
-    assumption.
-Qed.
-
-
-Lemma try_val_rec : forall p v l, try_valuation (v::l) p = Some v \/ try_valuation (v::l) p = try_valuation l p .
-Proof.
-  intros.
-  unfold try_valuation.
-  destruct (interp v p) eqn:E.
-  - now left.
-  - now right.
-Qed.
-  
-
-Lemma and_solver: forall p1 p2 V, find_valuation (land p1 p2)=Some V -> satisfiable p1.
-Proof.
-  intros.
-    unfold find_valuation in H.
-    unfold try_valuation in H.
-    induction (enum_valuation (collect_var (land p1 p2))). 
-    + discriminate H.
-    +  destruct (interp a (land p1 p2)) eqn: E1.
-       * unfold satisfiable.
-         exists a.
-         apply and_interp in E1.
-         destruct E1.
-         assumption.
-       * apply IHl. (*why it works?*)
-         apply H.
-Qed.*)
-
-Lemma and_solver': forall p1 p2 V, find_valuation (land p1 p2)=Some V -> interp V p1=true /\ interp V p2=true.
+Lemma land_find_interp: forall p1 p2 V, find_valuation (land p1 p2)=Some V -> interp V p1=true /\ interp V p2=true.
 Proof.
   intros.
   split;unfold find_valuation in H;
@@ -268,7 +262,7 @@ Proof.
     try(  destruct (interp a (land p1 p2)) eqn: E1);
     
        try(inversion H;
-         apply and_interp in E1;
+         apply land_interp_interp in E1;
          destruct E1;
          rewrite <-H1;
          assumption);
@@ -276,7 +270,7 @@ Proof.
          apply H).
 Qed.
 
-Lemma or_interp: forall p1 p2 V, interp V p1=true \/ interp V p2=true <-> interp V (lor p1 p2)=true.
+Lemma lor_interp_interp: forall p1 p2 V, interp V p1=true \/ interp V p2=true <-> interp V (lor p1 p2)=true.
 Proof.
   intros.
   split.
@@ -300,7 +294,7 @@ Proof.
 Qed.
 
 
-Lemma or_solver': forall p1 p2 V, find_valuation (lor p1 p2)=Some V -> interp V p1=true \/ interp V p2=true.
+Lemma lor_find_interp: forall p1 p2 V, find_valuation (lor p1 p2)=Some V -> interp V p1=true \/ interp V p2=true.
 Proof.
   intros.
   unfold find_valuation in H;
@@ -310,7 +304,7 @@ Proof.
     - destruct (interp a (lor p1 p2)) eqn: E1.
     
        + inversion H.
-         apply or_interp in E1.
+         apply lor_interp_interp in E1.
          rewrite <-H1;
            assumption.
          
@@ -319,22 +313,95 @@ Proof.
 Qed.
 
 
-(*
-Lemma test:forall (A B:Prop) (H1:A/\ B -> exists A, A-> B) (H2: A /\ B), A\/B.
+Lemma mapsto_interp_interp: forall p1 p2 V, interp V p1=false \/ interp V p2=true <-> interp V (mapsto p1 p2)=true.
 Proof.
   intros.
-  apply H1 in H2.
-  
-  assumption.
-Qed. 
-*) 
+  split.
+  - intro.
+    destruct H.
+     unfold interp.
+     unfold interp in H.
+     rewrite H. reflexivity.
+     unfold interp.
+     unfold interp in H.
+     rewrite H. apply Bool.orb_true_r.
+  - intro.
+
+    unfold interp in H.
+    Search orb.
+    apply Bool.orb_prop in H.
+    destruct H.
+    + left.
+      
+      unfold interp.
+      Search negb.
+      now apply Bool.negb_true_iff.
+    + right.
+      unfold interp.
+      assumption.
+Qed.
+
+Lemma mapsto_find_interp: forall p1 p2 V, find_valuation (mapsto p1 p2)=Some V -> interp V p1=false \/ interp V p2=true.
+Proof.
+  intros.
+  unfold find_valuation in H;
+    unfold try_valuation in H;
+    induction (enum_valuation (collect_var (mapsto p1 p2))).
+    - discriminate H.
+    - destruct (interp a (mapsto p1 p2)) eqn: E1.
+    
+       + inversion H.
+         apply mapsto_interp_interp in E1.
+         rewrite <-H1;
+           assumption.
+         
+     +   try(apply IHl; (*why it works?*)
+         apply H).
+Qed.
+
+
+Lemma not_interp_interp: forall p V, interp V p=false  <-> interp V (not p)=true.
+Proof.
+  intros.
+  split.
+  - intro.
+     unfold interp.
+     unfold interp in H.
+     rewrite H. reflexivity.
+  - intro.
+
+    unfold interp in H.
+
+    unfold interp.
+    now apply Bool.negb_true_iff.
+Qed.
+
+
+Lemma not_find_interp: forall p V, find_valuation (not p)=Some V -> interp V p=false.
+Proof.
+  intros.
+  unfold find_valuation in H;
+    unfold try_valuation in H;
+    induction (enum_valuation (collect_var (not p))).
+    - discriminate H.
+    - destruct (interp a (not p)) eqn: E1.
+    
+       + inversion H.
+         apply not_interp_interp in E1.
+         rewrite <-H1;
+           assumption.
+         
+     +   try(apply IHl; (*why it works?*)
+         apply H).
+Qed.
+
 (*
 Lemma and_satisfiable : forall p1 p2, satisfiable p1 -> satisfiable p2 -> satisfiable (land p1 p2).
 Proof.
   intros.
   unfold satisfiable in H, H0.
   unfold satisfiable.
-  setoid_rewrite  <-and_interp.
+  setoid_rewrite  <-land_interp_interp.
   destruct H,H0.
   
 didn't work because the lemma is false!
@@ -346,49 +413,44 @@ didn't work because the lemma is false!
 Lemma solver_sound : forall p, solver p = true -> satisfiable p.
 Proof.
   intros.
-  induction p.
-  - unfold satisfiable.
+  destruct p;unfold satisfiable;unfold solver in H.
+  - (*var*)
     exists (override empty_valuation i true).
     simpl.
     unfold override.
     now rewrite  <- beq_id_refl.
-  - unfold solver in H.
-    destruct (find_valuation (bool b)) eqn:E in H .
-    +  unfold find_valuation in E.
-       
-       simpl in E.
-       
-       destruct b.
-       
-       unfold satisfiable.       
-       now exists v.
-                  discriminate  E.
-                  +  discriminate H.
-                  -  (*land*)
-                    unfold satisfiable.
-                    
-                    unfold solver in H.
-                    destruct (find_valuation (land p1 p2)) eqn:E in H .
-                    +  
-                    exists v.
-                    apply and_interp.
-                    apply and_solver' in E.
-                    assumption.
-                  +
-                    discriminate H.
-
-                  - (*lor*)
-                    unfold satisfiable.
-                    
-                    unfold solver in H.
-                    destruct (find_valuation (lor p1 p2)) eqn:E in H .
-                    + exists v.
-                      apply or_interp.
-                      apply or_solver' in E.
-                      assumption.
-                    +  discriminate H.
-                    -  Admitted. (*TODO: mapsto*)   
-                    
+  - (*bool*)
+    destruct (find_valuation (bool b)) eqn:E in H;(try discriminate H).
+    unfold find_valuation in E.  
+    simpl in E.
+    destruct b.    
+    now exists v.
+    discriminate E.
+  -  (*land*)
+    destruct (find_valuation (land p1 p2)) eqn:E in H;(try discriminate H).
+    exists v.
+    apply land_interp_interp.
+    apply land_find_interp in E.
+    assumption.
+  - (*lor*)
+    destruct (find_valuation (lor p1 p2)) eqn:E in H;try(discriminate H).
+    exists v.
+    apply lor_interp_interp.
+    apply lor_find_interp in E.
+    assumption.
+  - (* mapsto*)   
+    destruct (find_valuation (mapsto p1 p2)) eqn:E in H;try(discriminate H).
+    exists v.
+    apply mapsto_interp_interp.
+    apply mapsto_find_interp in E.
+    assumption.
+  - (* not*)   
+    destruct (find_valuation (not p)) eqn:E in H;try(discriminate H).
+    exists v.
+    apply not_interp_interp.
+    apply not_find_interp in E.
+    assumption.
+Qed.      
                   
                   
 
